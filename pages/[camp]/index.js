8f7aff4160d2eb9de8dffff0b9c37b5314abe195
Sam Poder <39828164+sampoder@users.noreply.github.com>
2020-09-08 15:48:46 +0800

    bugzzz

--- pages/[camp]/index.js
+++ pages/[camp]/index.js
@@ -13,28 +13,6 @@ const Post = (props) => {
       <p>Ends at: {props.events[0].end}</p>
       <p>All times in {props.events[0].tz}</p>
       <p><a href={props.events[0].link}>Sign up</a></p>
-      <Grid.Container gap={2} style={{ overflow: "hidden" }} justify="center">
-      {props.events.map((event) => (
-        <Grid xs={6}>
-          <Card shadow style={{ width: "100%", marginBottom: "24px" }}>
-            <Image
-              src="https://images.unsplash.com/photo-1525625293386-3f8f99389edd?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1549&q=80"
-              height="100%"
-            />
-            <h4 style={{ marginBlockEnd: "0em" }}>{event.title}</h4>
-            <p style={{ marginBlockStart: "0.4em" }}>
-              From {event.start} to {event.end}, we'll be taking a trip to the
-              Lion City (which sadly does not have any lions).
-            </p>
-            <Card.Footer>
-              <Link color href={event.slug}>
-                Learn more and sign up.
-              </Link>
-            </Card.Footer>
-          </Card>
-        </Grid>
-      ))}
-    </Grid.Container>
     </>
 
   )

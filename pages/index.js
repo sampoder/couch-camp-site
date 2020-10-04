fd565b8201a76071799ed7d9f6088d65ca8f8f43
Sam Poder <39828164+sampoder@users.noreply.github.com>
2020-10-04 15:45:40 +0800

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -175,6 +175,7 @@ const Home = (props) => (
             <Button size="large" type="secondary">
               Find a team.
             </Button>{" "}</a>
+            <Spacer y={0.5} />
             <a href="https://cloud-5r403iznp.vercel.app/cc2_schedule.pdf">
             <Button size="large" type="secondary">
               View schedule.

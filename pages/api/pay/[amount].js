7e789646a06039406605f744f6ade233f1d9992c
Sam Poder <39828164+sampoder@users.noreply.github.com>
2020-10-08 19:54:12 +0800

    Create [amount].js

---
+++ pages/api/pay/[amount].js
@@ -0,0 +1,21 @@
+const stripe = require('stripe')(process.env.STRIPE_API_KEY);
+
+export default async (req, res) => {
+  const session = await stripe.checkout.sessions.create({
+    payment_method_types: ['card'],
+    line_items: [
+      {
+        name: 'Ticket to MUN @ GEMS',
+        description: 'A different kind of MUN conference.',
+        images: ['https://cloud-rili0t7tg.vercel.app/82225120_1255964828124592_8113147521939543579_n.jpg'],
+        amount: amount, // Cents
+        currency: 'sgd',
+        quantity: 1
+      }
+    ],
+    success_url: 'https://docs.google.com/forms/d/e/1FAIpQLSeRVHLBlWjNt4J25oU3zyLWMvCcFj8eUZV7Yg_HHtokOqYdLg/viewform?usp=pp_url&entry.1340452357={CHECKOUT_SESSION_ID}',
+    cancel_url: 'https://mun-gems.vercel.app'
+  });
+
+  return res.status(200).json(session);
+};

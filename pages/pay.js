60246fc6efce02f2fc1cb1569668b785279b818c
Sam Poder <39828164+sampoder@users.noreply.github.com>
2020-10-08 20:48:47 +0800

    wooooo its done

--- pages/pay.js
+++ pages/pay.js
@@ -1,4 +1,3 @@
-
 import Head from "next/head";
 import {
   Button,
@@ -19,12 +18,14 @@ const stripePromise = loadStripe(
   "pk_test_51HZxQPCmstuPJabcFQ2tqk8dHSfBp6FI3MhLKyN5URSZltTAEPNaIYRYXW7oFJR730g3WxTx0w45TIlOrl1eK07m00Ydqz5vlf"
 );
 
-const handleClick = async (event) => {
+
+
+const handleClick = async (event, amount) => {
   // Get Stripe.js instance
   const stripe = await stripePromise;
 
   // Call your backend to create the Checkout Session
-  const response = await fetch("/api/pay/5", {
+  const response = await fetch("https://couch.camp/api/pay/"+amount, {
     method: "POST",
   });
 
@@ -41,16 +42,66 @@ const handleClick = async (event) => {
   }
 };
 
-export default function Home() {
+const getValue = value => () => {
+  handleClick(value);
+};
 
+export default function Home() {
   return (
-  
-  <Button
-                      type="secondary"
-                      style={{ marginTop: "-1px" }}
-                      onClick={handleClick}
-                    >
-                      Register
-                    </Button>
-   );
+    <div style={{ textAlign: "center", marginTop: "80px", maxWidth: '800px', margin: 'auto' }}>
+      <h1>Pay for Couch Camp</h1>
+      <p>
+        Many countries don't have access to GoFundMe, you can use this form as a
+        proxy. All donations made here will go to the WSC GoFundMe. This was
+        created for Couch Camp. You will receive an email with a receipt, this
+        can be used as your payment proof.
+      </p>
+      <Button
+        type="secondary"
+        style={{ margin: "10px" }}
+        onClick={getValue(15)}
+      >
+        Donate the minimum $15
+      </Button>
+      <p>
+        The minimum amount is $15, however the WSC needs as much support as possible. If you can please consider donating more.
+      </p>
+      <Button
+        type="secondary"
+        style={{ margin: "10px" }}
+        onClick={getValue(30)}
+      >
+        Donate $30
+      </Button>
+      <Button
+        type="secondary"
+        style={{ margin: "10px" }}
+        onClick={getValue(40)}
+      >
+        Donate $40
+      </Button>
+      <Button
+        type="secondary"
+        style={{ margin: "10px" }}
+        onClick={getValue(50)}
+      >
+        Donate $50
+      </Button>
+      <Button
+        type="secondary"
+        style={{ margin: "10px" }}
+        onClick={getValue(70)}
+      >
+        Donate $70
+      </Button>
+      <Button
+        type="secondary"
+        style={{ margin: "10px" }}
+        onClick={getValue(100)}
+      >
+        Donate $100
+      </Button>
+      <p>Thank you for your kind support keeping the program we all love alive.</p>
+    </div>
+  );
 }
